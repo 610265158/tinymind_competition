fc1f3576792f681f9bc143c5cd5add9bff4aa53f
Lz <2120140200@mail.nankai.edu.cn>
2018-05-11 12:05:02 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,1 @@
-python train.py --num_classes 100 --finetune 1 --network resnext-50 --layer_name _plus14,_plus15,_plus13 --data_shape 128 --batch_size 100 --wd 0.005
-
+python train.py --num_classes 100 --finetune 1 --network resnext-50 --layer_name _plus13,_plus14 --data_shape 128 --batch_size  50 --wd 0.005
