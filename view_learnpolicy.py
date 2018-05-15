f12835a82c4fd314221ab0bb41e069aa619bea08
Lz <2120140200@mail.nankai.edu.cn>
2018-05-15 17:04:51 +0800

    Update view_learnpolicy.py

--- view_learnpolicy.py
+++ view_learnpolicy.py
@@ -1,20 +1,28 @@
 import matplotlib.pyplot as plt
+import math
 '''
 ####param:
-set 
-
 
 '''
+import argparse
+parser = argparse.ArgumentParser(description='Start view lr policy.')
+parser.add_argument('--base_lr', dest='base_lr',type=float, default=0.01,  \
+                    help='')
+parser.add_argument('--max_update', dest='max_update',type=int, default=128,  \
+                    help='')
+parser.add_argument('--power', dest='power',type=float, default=3,  \
+                    help='')
 
+args = parser.parse_args()
 print('visualize the lr lr_scheduler, choose a ideal one,')
 
 ###lr_scheduler = mx.lr_scheduler.PolyScheduler(8000,0.005, 2)
 
 
-max_update=8000
+max_update=args.max_update
 
-base_lr_orig=0.01
-power=3
+base_lr_orig=args.base_lr
+power=args.power
 x=[]
 y=[]
 
@@ -26,3 +34,4 @@ for num_update in range(max_update):
 
 plt.plot(x,y)
 plt.show()
+
